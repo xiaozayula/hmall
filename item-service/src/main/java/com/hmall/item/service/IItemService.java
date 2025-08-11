1c58f657a10471a87060ab5340c6710e92950a1b
xiaozayula <981446636@qq.com>
2025-08-11 15:49:16 +0800

    item商品模块

---
+++ item-service/src/main/java/com/hmall/item/service/IItemService.java
@@ -0,0 +1,16 @@
+package com.hmall.item.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmall.item.domain.dto.ItemDTO;
+import com.hmall.item.domain.dto.OrderDetailDTO;
+import com.hmall.item.domain.po.Item;
+import java.util.Collection;
+import java.util.List;
+
+
+public interface IItemService extends IService<Item> {
+
+    void deductStock(List<OrderDetailDTO> items);
+
+    List<ItemDTO> queryItemByIds(Collection<Long> ids);
+}
