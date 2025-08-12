58e46807770feee926d5a67fcd75ded563590431
xiaozayula <981446636@qq.com>
2025-08-12 10:56:22 +0800

    cart模块调用item模块-openfeign优化

--- cart-service/src/main/java/com/hmall/cart/CartApplication.java
+++ cart-service/src/main/java/com/hmall/cart/CartApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 
-@EnableFeignClients
+@EnableFeignClients(basePackages = "com.hmall.api.client")
 @MapperScan("com.hmall.cart.mapper")
 @SpringBootApplication
 public class CartApplication {
